d7484e70391dc73e61e708efdc1f6c11aec5c407
CoderRohan003 <rohandutta756@gmail.com>
2024-03-06 02:49:31 +0530

    Final Commit

--- controllers/url.js
+++ controllers/url.js
@@ -11,7 +11,7 @@ async function handleGenerateNewShortURL(req,res){
         redirectURL:body.url,
         visitHistory: [],
     });
-    return res.json({ id: shortId });
+    return res.render('home', {id:shortId});
 }
 
 async function handleGetAnalytics(req,res){
